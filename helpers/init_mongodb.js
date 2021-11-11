1a5554ff88a988157797dd888d0bfa7b5f90be1c
Mi5-hub <mino.randimbiarilanto@gmail.com>
2021-11-11 13:35:45 +0300

    first commit

---
+++ helpers/init_mongodb.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const uri = "mongodb+srv://saynatest:saynatest@cluster0.rpee7.mongodb.net/regist?retryWrites=true&w=majority"
+
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    dbName: process.env.DB_NAME,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('mongodb connected.')
+  })
+  .catch((err) => console.log(err.message))
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to db')
+})
+
+mongoose.connection.on('error', (err) => {
+  console.log(err.message)
+})
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose connection is disconnected.')
+})
+
+process.on('SIGINT', async () => {
+  await mongoose.connection.close()
+  process.exit(0)
+})
